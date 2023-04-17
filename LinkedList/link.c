d7576ba327688fcf56c4cc2e731aabfb41d5e996
X-Cyborg <puneethkshetty6720@gmail.com>
2023-04-17 18:05:03 +0530

    Update link.c

--- LinkedList/link.c
+++ LinkedList/link.c
@@ -18,14 +18,37 @@ void delete(Node** head,int key)
 {
    if(head == NULL)
      return;
-   Node* temp = *head;
   
+   Node* temp = *head,*prev;
+   if(temp!=null && temp->data == key)
+   { 
+     *head = temp->next;
+     free(temp);
+     return;
+   }
+  
+  while(temp!=null && temp->data != key)
+  {
+    prev = next;
+    temp = temp->next;
+  }
+  
+  prev->next = temp->next;
+  free(temp);
  
 }
 
 int main()
 {
-   struct Node* head; 
+   struct Node* head = NULL;
+   push(&head,1);
+   push(&head,2);
+   push(&head,3);
+   push(&head,4);
+  
+   delete(&head,1);
+  
+   
    
    return 0;
 }
