aaa509d3d72e28c7c7b354cde9bd31f43bb28ea9
X-Cyborg <puneethkshetty6720@gmail.com>
2023-04-19 15:28:17 +0530

    Update link.c

--- LinkedList/link.c
+++ LinkedList/link.c
@@ -1,54 +1,70 @@
+#include "Linkedlist.h"
+#include <stdio.h>
+#include <stdlib.h>
 
-typedef struct Node
-{
-  int data;
-  struct Node* next;
-}Node;
-
-
-void insert(Node** head,int new_data)
-{
-   Node* new_node = malloc(sizeof(Node));
-   new_node->data = new_data;
-   new_node->next = (*head_ref);
-   *head = new_node;
+struct Node* createNode(int data) {
+    struct Node* newNode = (struct Node*)malloc(sizeof(struct Node));
+    newNode->data = data;
+    newNode->next = NULL;
+    return newNode;
 }
 
-void delete(Node** head,int key)
-{
-   if(head == NULL)
-     return;
-  
-   Node* temp = *head,*prev;
-   if(temp!=null && temp->data == key)
-   { 
-     *head = temp->next;
-     free(temp);
-     return;
-   }
-  
-  while(temp!=null && temp->data != key)
-  {
-    prev = next;
-    temp = temp->next;
-  }
-  
-  prev->next = temp->next;
-  free(temp);
- 
+void printList(struct Node* head) {
+    struct Node* temp = head;
+    while (temp != NULL) {
+        printf("%d ", temp->data);
+        temp = temp->next;
+    }
+    printf("\n");
 }
 
-int main()
-{
-   struct Node* head = NULL;
-   push(&head,1);
-   push(&head,2);
-   push(&head,3);
-   push(&head,4);
-  
-   delete(&head,1);
-  
-   
-   
-   return 0;
+int main() {
+    struct Node* head = NULL;
+    int data,i;
+
+    while(1) {
+
+    // take user input for the size
+    printf("Enter the size of the linked list (not more than 4): ");
+    scanf("%d", &size);
+
+    // delete nodes if size is negative
+    if (size < 0) {
+        size = -size;
+        for (i = 0; i < size; i++) {
+            if (head == NULL) {
+                printf("Cannot delete more nodes. Linked list is empty.\n");
+                break;
+            }
+            struct Node* temp = head;
+            head = head->next;
+            free(temp);
+        }
+    }
+    // add nodes if size is positive
+    else if (size >= 0  & size <= 4) {
+        for (i = 0; i < size; i++) {
+            printf("Enter data for node %d: ", i+1);
+            scanf("%d", &data);
+            struct Node* newNode = createNode(data);
+            if (head == NULL) {
+                head = newNode;
+            }
+            else {
+                struct Node* temp = head;
+                while (temp->next != NULL) {
+                    temp = temp->next;
+                }
+                temp->next = newNode;
+            }
+        }
+    }
+
+    // print the final linked list
+    printf("Final linked list: ");
+    printList(head);
+
+    }
+
+    return 0;
 }
