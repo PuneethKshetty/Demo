c3e4492f53e892aca5cb5331695ad8da59e7b6a3
X-Cyborg <puneethkshetty6720@gmail.com>
2023-04-19 15:28:46 +0530

    Update print.c

--- Print/print.c
+++ Print/print.c
@@ -1,2 +1,37 @@
 //print anything if that state
+#include "StateMachine.h"
+#include "StatePrinting.h"
+#include "Linkedlist.h"
+#include <stdio.h>
 
+void printState(State currentState) {
+    switch (currentState) {
+        case STATE_A:
+            printf("State: A\n");
+            break;
+        case STATE_B:
+            printf("State: B\n");
+            break;
+        case STATE_C:
+            printf("State: C\n");
+            break;
+        default:
+        printf("No state here");
+        break;
+    }
+}
+
+int main()
+{
+    State currentState = STATE_A;
+    int input;
+    while (1) {
+        printf("Enter an integer input (or 0 to exit): ");
+        scanf("%d", &input);
+        if (input == 0) {
+            break;
+        }
+        currentState = transition(currentState, input);
+        printState(currentState);
+    }
+}
